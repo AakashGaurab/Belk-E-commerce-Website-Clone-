3c075144bdbef2abcae38647d9efc66a2f10af86
Aakash <aakashgaurav456@gmail.com>
2023-01-19 09:03:00 +0530

    Completed Day2 Works

--- backend/models/products_model.js
+++ backend/models/products_model.js
@@ -4,10 +4,11 @@ const product_schema=mongoose.Schema({
     img:String,
     title:String,
     description:String,
-    price:Number,
+    price:String,
     category:String
 })
 
 const product_model=mongoose.model("products",product_schema);
-
-module.exports={product_model}
+const female_model=mongoose.model("female",product_schema);
+const male_model=mongoose.model("male",product_schema);
+module.exports={product_model,female_model,male_model};
