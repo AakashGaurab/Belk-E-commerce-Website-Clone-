a50f64aac2c3e302d5e36be92f240f886e060461
Aakash <aakashgaurav456@gmail.com>
2023-01-20 21:32:19 +0530

    Day4 Works

--- backend/models/products_model.js
+++ backend/models/products_model.js
@@ -5,10 +5,13 @@ const product_schema=mongoose.Schema({
     title:String,
     description:String,
     price:String,
-    category:String
+    category:String,
+    user_id:String
 })
 
 const product_model=mongoose.model("products",product_schema);
 const female_model=mongoose.model("female",product_schema);
 const male_model=mongoose.model("male",product_schema);
-module.exports={product_model,female_model,male_model};
+const cart_model=mongoose.model("cart",product_schema);
+//const cart_model=1
+module.exports={product_model,female_model,male_model,cart_model};
--- backend/routes/products_routes.js
+++ backend/routes/products_routes.js
@@ -1,35 +1,49 @@
 const express=require("express");
 const product=express.Router();
-const {product_model,female_model,male_model}=require("../models/products_model");
-
+const {product_model,female_model,male_model,cart_model}=require("../models/products_model");
+//get product for all user 
 product.get("/",async (req,res)=>{
     let data = await product_model.find({});
 
     res.json(data);
 })
-
+// post product for admin
 product.post("/add",async (req,res)=>{
-    if (req.body.category=="female"){
-        let payload = req.body;
-        try {
-            await female_model.insertMany([payload]);
-            res.json("product uploaded")
-        } catch (error) {
-           res.json("Error uploading product") 
+    if (req.email=="admin@gmail.com"){
+        if (req.body.category=="female"){
+            let payload = req.body;
+            try {
+                await female_model.insertMany([payload]);
+                res.json("product uploaded")
+            } catch (error) {
+               res.json("Error uploading product") 
+            }
+        }
+        else if (req.body.category=="male"){
+            let payload = req.body;
+            try {
+                await male_model.insertMany([payload]);
+                res.json("product uploaded")
+            } catch (error) {
+               res.json("Error uploading product") 
+            }
         }
     }
-    else if (req.body.category=="male"){
-        let payload = req.body;
-        try {
-            await male_model.insertMany([payload]);
-            res.json("product uploaded")
-        } catch (error) {
-           res.json("Error uploading product") 
-        }
+    else {
+        res.json("You are not authorised")
     }
+   
      
 })
+//  post for user to add to cart
+product.post("/cart",(req,res)=>{
+    let pay = req.body;
+    console.log(pay)
+    res.send("Thanks");
+})
 
+
+//delete section for admin
 product.delete("/",async (req,res)=>{
     await male_model.deleteMany({});
     res.send("Data Deleted ") 
