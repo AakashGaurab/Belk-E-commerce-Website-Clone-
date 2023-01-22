c494e39953e8b5f4839d02e416f0a394e83f133b
Aakash <aakashgaurav456@gmail.com>
2023-01-22 15:11:03 +0530

    Ui And responsive created

--- frontend/landing_page.js
+++ frontend/landing_page.js
@@ -2,3 +2,14 @@ document.querySelector("#search_button").addEventListener("click",()=>{
     let querry = document.querySelector(".search_bar").value;
     window.location.href="product.html";
 })
+
+document.querySelector(".logo").addEventListener("click",()=>{
+    window.location.href="landing_page.html"
+})
+
+let all_img=document.querySelectorAll("body img")
+for (let i=0;i<all_img.length;i++){
+    all_img[i].addEventListener("click",()=>{
+        window.location.href="product.html";
+    })
+}
